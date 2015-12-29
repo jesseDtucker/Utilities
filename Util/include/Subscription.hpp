e80eb4ce3fd42a670337e040bd6db45a53ed5d3d
jesseDtucker <jesseDtucker@gmail.com>
2015-12-28 23:15:16 -0700

    Updated with latest utilities

--- Util/include/Subscription.hpp
+++ Util/include/Subscription.hpp
@@ -1,22 +1,33 @@
 #ifndef SUBSCRIPTION_HPP
 #define SUBSCRIPTION_HPP
 
+#include <memory>
+
 #include "Delegate.hpp"
 
-namespace Util
-{
-	class Subscription final
-	{
-	public:
-		typedef std::function<void()> UnsubscribeCallback;
-		Subscription(UnsubscribeCallback unsubscribeCallback);
-		~Subscription();
+namespace Util {
+class Subscription;
 
-		void Unsubscribe();
-	private:
-		bool m_isSubscribed = true;
-		UnsubscribeCallback m_unsubscribeCallback;
-	};
+typedef std::unique_ptr<Subscription> SubscriptionPtr;
+
+class Subscription final {
+ public:
+  typedef std::function<void()> UnsubscribeCallback;
+  Subscription();
+  Subscription(std::nullptr_t);
+  Subscription(UnsubscribeCallback unsubscribeCallback);
+  Subscription(Subscription&&);
+  Subscription& operator=(Subscription&&);
+  Subscription(const Subscription&) = delete;  // copy is not allowed. If needs to be shared please place in shared_ptr
+  Subscription& operator=(const Subscription&) = delete;
+  ~Subscription();
+
+  void Unsubscribe();
+
+ private:
+  bool m_isSubscribed = true;
+  UnsubscribeCallback m_unsubscribeCallback;
+};
 }
 
 #endif
